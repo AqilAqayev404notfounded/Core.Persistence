53b33eb68f40b6d311a8107026ec5d5646bb3b6a
AgilDeveloper <157524718+AqilAqayev404notfounded@users.noreply.github.com>
2024-10-28 18:30:25 +0400

    Add project files.

---
+++ Core.Persistence/Repositories/EfRepositoryBase.cs
@@ -0,0 +1,72 @@
+﻿using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Core.Persistence.Repositories
+{
+    public class EfRepositoryBase<TEntity, TContext> : IRepositoryAsync<TEntity>
+        where TEntity : Entity
+        where TContext : DbContext
+    {
+        protected TContext Context { get; }
+
+        public EfRepositoryBase(TContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            var entityEntry = await Context.Set<TEntity>().AddAsync(entity);
+            await Context.SaveChangesAsync();
+
+            return entityEntry.Entity;
+        }
+
+        public async Task<TEntity> DeleteAsync(TEntity entity)
+        {
+            var entityEntry = Context.Set<TEntity>().Remove(entity);
+            await Context.SaveChangesAsync();
+
+            return entityEntry.Entity;
+        }
+
+        public async Task<TEntity?> GetAsync(int id)
+        {
+            return await Context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null)
+        {
+            var query = Context.Set<TEntity>().AsQueryable();
+
+            if (include != null) query = include(query);
+
+            return await query.FirstOrDefaultAsync(predicate);
+        }
+
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            var entityEntry =  Context.Set<TEntity>().Update(entity);
+            await Context.SaveChangesAsync();
+
+            return entityEntry.Entity;
+        }
+
+        public async Task<Paginate<TEntity?>> GetListAsync(Expression<Func<TEntity, bool>>? predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderby = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+            int index = 0, int size = 10, bool enableTracking = true)
+        {
+            IQueryable<TEntity> queryable = Context.Set<TEntity>();
+            if (!enableTracking) queryable = queryable.AsNoTracking();
+            if (include != null) queryable = include(queryable);
+            if (predicate != null) queryable = queryable.Where(predicate);
+            if (orderby != null) queryable = orderby(queryable);
+
+            return await queryable.ToPaginateAsync(index , size);
+        }
+    }
+}
